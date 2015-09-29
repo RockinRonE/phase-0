acff546311c7f2bf5eb08c35d42fad27101702ed
Ronny Ewanek <ronny.ewanek@gmail.com>
2015-09-29 13:44:36 -0500

    finish 4.3.1

---
+++ week-4/address/my_solution.rb
@@ -0,0 +1,20 @@
+# Format an Address
+
+
+
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+
+def make_address (street, city, state, zip)
+
+	 "You live at " + street + ", in the beautiful city of " + city + ", " + state + ". Your zip is " + zip + "."
+end
+
+#For some reason this wouldn't pass rspec with puts before the string. Removing puts allowed it to pass BUT now the #program won't run
+
+
+#make_address("633 Folsom St.", "San Francisco", "CA", "04107")
