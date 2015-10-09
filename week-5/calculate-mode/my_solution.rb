436c3054f776b1c598970f8a4af110ff1ce55680
Ronny Ewanek <ronny.ewanek@gmail.com>
2015-10-09 14:22:34 -0500

    finish 5.3 calculate the mode

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -1,8 +1,8 @@
 # Calculate the mode Pairing Challenge
 
-# I worked on this challenge [by myself, with: ]
+# I worked on this challenge with: Josh Abrams
 
-# I spent [] hours on this challenge.
+# I spent 3 hours on this challenge.
 
 # Complete each step below according to the challenge directions and
 # include it in this file. Also make sure everything that isn't code
@@ -12,41 +12,72 @@
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? An array
+# What is the output? An Array
 # What are the steps needed to solve the problem?
 
 
 # 1. Initial Solution
 
 def mode(array)
-  hash = Hash.new(0) #all keys will have a value of 0
-  
-  
-  array.each {|x| hash[x] += 1 }
-  #sorts by the key 
-  final_hash = hash.sort_by {|k,v| k }
-  
-  
-  
-  final_hash.sort_by do |k,v| 
-    v 
-  end
-  
-  p final_hash.reverse!
-  
-  
-  #hashes within an array? 
-#   final_hash.reverse
+    #create hash and set default value to 0
+    hash = Hash.new(0)
+   	# create an array to store our answer
+    new_array = []
+    # iterate over the array and add to key. Hashes can only have one key so every time there's a duplicate key, 
+    # value will still increase
+    array.each { |x| hash[x] += 1 }
+     
+     #find the maximum value of the keys 
+    maximum = hash.values.max
+    
+    # interate over the hash and compare the value to the max value 
+    hash.each do |key, value|
+      #this checking for equal occurance 
+       if value == maximum
+         new_array << key
+       end
+     
+     end
+   return new_array 
 end
-
-mode(["bob", "bob", "sam", "sam", "sam", "chris"])
-
-
-
-# 3. Refactored Solution
-
-
 
 
 # 4. Reflection
+=begin
+Which data structure did you and your pair decide to implement and why?
+
+	Hashes worked well since you couldn't have duplicate keys. It was neat to use this to have it count values from an array.
+
+Were you more successful breaking this problem down into implementable pseudocode than the last with a pair? 
+
+	Sort of. This exercise exposed a lot of ignorance about hashes that I had. I learned a lot :)
+
+What issues/successes did you run into when translating your pseudocode to code?
+
+	I had a hard time wrapping my head around the "hash[x] += 1" part. I needed to keep in mind that we set the default value to 0 and that we couldn't have duplicate keys. Everytime we did, it was add 1 to the value but not the key. 
+
+What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+
+	I didn't find any other unknown methods since each allowed us to access both the key and value. 
+
+=end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
