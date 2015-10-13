7e9af4bd61907d689570f0106a56d7c07396dbc1
Ronny Ewanek <ronny.ewanek@gmail.com>
2015-10-13 11:10:49 -0500

    initial commit

---
+++ week-6/attr/my_solution.rb
@@ -0,0 +1,79 @@
+#Attr Methods
+
+# I worked on this challenge by myself
+
+# I spent [#] hours on this challenge.
+
+class NameData
+
+end
+
+
+class Greetings
+
+end
+
+
+
+# Reflection
+=begin
+
+Release 1: Read and Research
+
+What are these methods doing?
+
+	These methods allow us to update information and print it. 
+
+How are they modifying or returning the value of instance variables?
+
+	They are using methods to update the values. 
+
+------------------------------------------------------------------------
+
+Release 2: Identify the Changes
+
+What changed between the last release and this release?
+
+	We are using an attribute reader to simply our class
+
+What was replaced?
+
+	We replaced the what_is_age method with an attribute reader
+
+Is this code simpler than the last?
+	
+	Definitely. 
+
+------------------------------------------------------------------------
+
+Release 3: More Changes
+
+What changed between the last release and this release?
+
+	We're now using a attribute writer
+
+What was replaced?
+
+	We replaced a method that update the age with an attribute writer
+
+Is this code simpler than the last?
+
+	Definitely.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
