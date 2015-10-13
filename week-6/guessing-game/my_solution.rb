d6645607fcbf1d9718772c7e28d3c271729e0829
Ronny Ewanek <ronny.ewanek@gmail.com>
2015-10-13 15:50:32 -0500

    finish refelection

--- week-6/guessing-game/my_solution.rb
+++ week-6/guessing-game/my_solution.rb
@@ -2,7 +2,7 @@
 
 
 # I worked on this challenge by myself
-# I spent 1.5 hours on this challenge.
+# I spent 3 hours on this challenge.
 
 # Pseudocode
 
@@ -13,44 +13,35 @@
 
 # Initial Solution
 
-class GuessingGame
+# class GuessingGame
 
-  def initialize(answer)
-    # Your initialization code goes here
-    @answer = answer
+#   def initialize(answer)
+#     # Your initialization code goes here
+#     @answer = answer
 
-  end
+#   end
 
-  def guess(integer)
-    # INPUT: integer
-    @status = integer
-    if integer < @answer
-      :low
-    elsif integer > @answer
-      :high
-    else 
-      :correct
-    end
+#   def guess(integer)
+#     @status = integer
+#     if integer < @answer
+#       :low
+#     elsif integer > @answer
+#       :high
+#     else
+#       :correct
+#     end
 
-  end
+#   end
 
-  def solved?
-    if @status == @answer
-      true
-    else
-      false
-    end
-  end
-end
+#   def solved?
+#     if @status == @answer
+#       true
+#     else
+#       false
+#     end
+#   end
+# end
 
-
-
-game = GuessingGame.new(10)
-p game.guess(4)
-
-p game.solved?
-
-# Make sure you define the other required methods, too
 
 
 
@@ -59,8 +50,59 @@ p game.solved?
 # Refactored Solution
 
 
+class GuessingGame
 
+  def initialize(answer)
+    @answer = answer
+  end
 
+  def guess(integer)
+    @status = integer
 
+    integer > @answer ? :high : integer < @answer ? :low :  :correct
+
+  end
+
+  def solved?
+    @status.eql? @answer
+  end
+
+end
+
+game = GuessingGame.new(10)
+p game.guess(10)
+
+p game.solved?
 
 # Reflection
+=begin
+How do instance variables and methods represent the characteristics and behaviors (actions) of a real-world object?
+
+  Within a class (like a real-world object) you can define methods to mimick their characteristics and behaviors.
+
+When should you use instance variables? What do they do for you?
+
+  Instance variables are variables taht are shared amongst methods.
+
+Explain how to use flow control. Did you have any trouble using it in this challenge? If so, what did you struggle with?
+
+    I didn't have any trouble, BUT I did struggle to refactor.
+
+Why do you think this code requires you to return symbols? What are the benefits of using symbols?
+
+  By using symbols we don't have to use String methods. That's about the only benefit that I can think of for this application. 
+
+
+=end
+
+
+
+
+
+
+
+
+
+
+
+
