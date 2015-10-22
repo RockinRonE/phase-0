6dc8cf36d6182ee095bbe99a714d90f5e5fd8ccb
Ronny Ewanek <ronny.ewanek@gmail.com>
2015-10-22 15:18:11 -0500

    commit to switch branches

---
+++ week-7/game.js
@@ -0,0 +1,112 @@
+
+// Design Basic Game Solo Challenge
+
+// This is a solo challenge
+
+// Your mission description: Battleship! To destroy opponents ship first. 
+// Overall mission:
+// Goals:
+// Characters: Two battleships
+// Objects: two battleships
+// Functions:
+
+// Pseudocode
+//create two battleship objects
+//place them randomly on board
+//
+//
+//
+
+// Initial Code
+
+
+
+
+
+
+// Refactored Code
+
+
+
+
+
+
+// Reflection
+//
+//
+//
+//
+//
+//
+//
+//
+
+ 
+var yourBase = {
+
+  posX: Math.floor((Math.random()*10)+1),
+  posY: Math.floor((Math.random()*10)+1),
+  destroyed: false,
+
+  fire: function(x, y) {
+    if(x === enemyBase.posX && y === enemyBase.posY) {
+      console.log("You hit the enemy base!");
+    }
+    else  {
+      console.log("You are at x: " + yourBase.posX + " and y: " + yourBase.posY);
+    }
+ }  
+};
+
+var enemyBase = {
+  posX: Math.floor((Math.random()*10)+1),
+  posY: Math.floor((Math.random()*10)+1),
+  destroyed: false,
+
+  
+
+   fire: function() {
+
+  var fireBackX = Math.floor((Math.random()*10)+1);
+  var fireBackY = Math.floor((Math.random()*10)+1);
+
+    if(enemyBase.fireBackX === yourBase.posX && enemyBase.fireBackY === yourBase.posY) {
+      console.log("You've been hit!");
+    }
+    else {
+      //console.log("The enemy is at x: " + yourBase.posX + " and y: " + yourBase.posY);
+    }
+ } 
+
+  
+};
+
+
+yourBase.fire(2,4);
+
+console.log(enemyBase.fire()); 
+// yourBase.fire(4,4);
+// yourBase.fire(3,4);
+
+
+var battling = false;//true; 
+
+
+
+
+totalDamage = 0; 
+
+while(battling) {
+  //if your number is equal to enemy number
+    //you hit
+  //else 
+    //nope
+
+  battling = false; 
+
+
+
+
+}
+
+
