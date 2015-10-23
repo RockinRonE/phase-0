a80c73b94608427f151abed395c4fe26ea74c56a
Ronny Ewanek <ronny.ewanek@gmail.com>
2015-10-23 16:25:50 -0500

    before adding do to while loop

--- week-7/game.js
+++ week-7/game.js
@@ -1,92 +1,88 @@
 
-// Design Basic Game Solo Challenge
-
-// This is a solo challenge
-
-// Your mission description: Battleship! To destroy opponents ship first. 
-// Overall mission:
-// Goals:
-// Characters: Two battleships
-// Objects: two battleships
-// Functions:
-
-// Pseudocode
-//create two battleship objects
-//place them randomly on board
-//
-//
-//
-
-// Initial Code
-
-
-
-
-
-
-// Refactored Code
-
-
-
-
-
-
-// Reflection
-//
-//
-//
-//
-//
-//
-//
-//
-
  
-var yourBase = {
+var myBase = {
 
-  posX: Math.floor((Math.random()*10)+1),
-  posY: Math.floor((Math.random()*10)+1),
-  destroyed: false,
-
-  fire: function(x, y) {
-    if(x === enemyBase.posX && y === enemyBase.posY) {
-      console.log("You hit the enemy base!");
-    }
-    else  {
-      console.log("You are at x: " + yourBase.posX + " and y: " + yourBase.posY);
-    }
- }  
-};
+  posX: Math.floor((Math.random()*5)+1),
+  posY: Math.floor((Math.random()*5)+1),
+  health: 5
+ };
 
 var enemyBase = {
-  posX: Math.floor((Math.random()*10)+1),
-  posY: Math.floor((Math.random()*10)+1),
-  destroyed: false,
-
-  
-
-   fire: function() {
-
-  var fireBackX = Math.floor((Math.random()*10)+1);
-  var fireBackY = Math.floor((Math.random()*10)+1);
-
-    if(enemyBase.fireBackX === yourBase.posX && enemyBase.fireBackY === yourBase.posY) {
-      console.log("You've been hit!");
-    }
-    else {
-      //console.log("The enemy is at x: " + yourBase.posX + " and y: " + yourBase.posY);
-    }
- } 
-
-  
+  posX: Math.floor((Math.random()*5)+1),
+  posY: Math.floor((Math.random()*5)+1),
+  health: 5
 };
 
+//Reveals base locations
+// var myPosition = myBase.posX;
+// for(var x in myBase) {
+// 	console.log(myBase[x]);
+// }
 
-yourBase.fire(2,4);
+// var enemyPosition = enemyBase.posX;
+// for(var x in enemyBase) {
+// 	console.log(enemyBase[x]);
+// }
 
-console.log(enemyBase.fire()); 
-// yourBase.fire(4,4);
-// yourBase.fire(3,4);
+var launch = function(x, y) {
+	if(x === enemyBase.posX && x === enemyBase.posY) {
+		enemyBase.health -= 1; 
+		console.log("You hit the base!");
+	} else {
+		console.log("You missed!"); 
+	}
+};
+
+var enemyLaunch = function() {
+
+	enemyLaunchCordX = Math.floor((Math.random()*5)+1);
+	enemyLaunchCordY = Math.floor((Math.random()*5)+1);
+
+	if(enemyLaunchCordX === myBase.posX && enemyLaunchCordY === myBase.posY) {
+		myBase.health -= 1;
+		console.log("You've been hit!");
+	} else {
+		console.log("That was a close one!"); 
+	}
+};
+
+var counter = 0;
+do {
+	
+	myBase.posX = Math.floor((Math.random()*5)+1);
+	myBase.posY = Math.floor((Math.random()*5)+1);
+
+	enemyBase.posX = Math.floor((Math.random()*5)+1);
+	enemyBase.posY = Math.floor((Math.random()*5)+1);
+}
+
+while ( myBase.health > 0 && enemyBase.health > 0) {
+	
+	launch(2,5); 
+	enemyLaunch();
+	counter += 1;
+
+}
+
+
+
+console.log(counter);
+// launch(2,5); 
+// enemyLaunch();
+// console.log(myBase);
+// console.log(enemyBase);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
