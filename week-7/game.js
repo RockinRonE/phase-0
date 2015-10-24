5564f0e3b5c6c30cb3814f180306c512e936c54a
Ronny Ewanek <ronny.ewanek@gmail.com>
2015-10-24 15:51:14 -0500

    time boxed game

--- week-7/game.js
+++ week-7/game.js
@@ -1,16 +1,14 @@
-
- 
 var myBase = {
 
-  posX: Math.floor((Math.random()*5)+1),
-  posY: Math.floor((Math.random()*5)+1),
-  health: 5
- };
+    posX: Math.floor((Math.random() * 5) + 1),
+    posY: Math.floor((Math.random() * 5) + 1),
+    health: 5
+};
 
 var enemyBase = {
-  posX: Math.floor((Math.random()*5)+1),
-  posY: Math.floor((Math.random()*5)+1),
-  health: 5
+    posX: Math.floor((Math.random() * 5) + 1),
+    posY: Math.floor((Math.random() * 5) + 1),
+    health: 5
 };
 
 //Reveals base locations
@@ -25,64 +23,45 @@ var enemyBase = {
 // }
 
 var launch = function(x, y) {
-	if(x === enemyBase.posX && x === enemyBase.posY) {
-		enemyBase.health -= 1; 
-		console.log("You hit the base!");
-	} else {
-		console.log("You missed!"); 
-	}
+    if (x === enemyBase.posX && x === enemyBase.posY) {
+        enemyBase.health -= 1;
+        console.log("You hit the base!");
+    } else {
+        console.log("You missed!");
+    }
 };
 
 var enemyLaunch = function() {
 
-	enemyLaunchCordX = Math.floor((Math.random()*5)+1);
-	enemyLaunchCordY = Math.floor((Math.random()*5)+1);
+	var pastLaunches = [];
 
-	if(enemyLaunchCordX === myBase.posX && enemyLaunchCordY === myBase.posY) {
-		myBase.health -= 1;
-		console.log("You've been hit!");
-	} else {
-		console.log("That was a close one!"); 
-	}
+    enemyLaunchCordX = Math.floor((Math.random() * 5) + 1);
+    enemyLaunchCordY = Math.floor((Math.random() * 5) + 1);
+
+    
+
+
+
+    if (enemyLaunchCordX === myBase.posX && enemyLaunchCordY === myBase.posY) {
+        myBase.health -= 1;
+        console.log("You've been hit!");
+    } else {
+        console.log("That was a close one!");
+    }
 };
 
-var counter = 0;
-do {
-	
-	myBase.posX = Math.floor((Math.random()*5)+1);
-	myBase.posY = Math.floor((Math.random()*5)+1);
 
-	enemyBase.posX = Math.floor((Math.random()*5)+1);
-	enemyBase.posY = Math.floor((Math.random()*5)+1);
-}
+var battle = function() {
 
-while ( myBase.health > 0 && enemyBase.health > 0) {
-	
-	launch(2,5); 
-	enemyLaunch();
-	counter += 1;
+    while (myBase.health > 0 && enemyBase.health > 0) {
+    	
+    	console.log(myBase.posX + " " + myBase.posY);
+        launch(2, 5);
+        enemyLaunch();
+        
 
-}
+    }
+};
 
 
-
-console.log(counter);
-// launch(2,5); 
-// enemyLaunch();
-// console.log(myBase);
-// console.log(enemyBase);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+battle();
